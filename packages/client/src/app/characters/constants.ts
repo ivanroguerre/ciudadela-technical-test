3f0205b05fff476d50e507dde5e5db24948cd57c
Iván Guerrero <ivanguebri@gmail.com>
2023-12-20 05:17:56 -0500

    Add the server endpoint as a constant

--- packages/client/src/app/characters/constants.ts
+++ packages/client/src/app/characters/constants.ts
@@ -1,3 +1,4 @@
+export const HTTP_SERVER_URL = "http://127.0.0.1:3001";
 // La máxima cantidad de documentos por página es fija y
 // se sabe a partir de la documentación en:
 // https://rickandmortyapi.com/documentation/#info-and-pagination
