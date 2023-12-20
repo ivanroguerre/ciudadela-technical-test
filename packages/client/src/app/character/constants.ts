d704c42c0ab6a4110a6fc386658fcc7f99f9b981
Iván Guerrero <ivanguebri@gmail.com>
2023-12-20 03:56:25 -0500

    Add constants file
    For character related info

---
+++ packages/client/src/app/character/constants.ts
@@ -0,0 +1,4 @@
+// La máxima cantidad de documentos por página es fija y
+// se sabe a partir de la documentación en:
+// https://rickandmortyapi.com/documentation/#info-and-pagination
+export const MAX_DOCUMENTS_PER_PAGE = 20;
