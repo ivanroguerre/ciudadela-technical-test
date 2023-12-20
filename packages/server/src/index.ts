50e0f9a38ec783622ed1c807aa8cda1de9bfb811
Iván Guerrero <ivanguebri@gmail.com>
2023-12-20 05:16:27 -0500

    Setup the express server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,47 @@
-import express from "express";
+import { CharactersResponse, CountResponse } from "shared";
+import express, { Response } from "express";
+import { parse } from "qs";
+
+import {
+  humansCountQuery,
+  humansQuery,
+  pagePlaceholder,
+  port,
+  rickAndMortyGraphQLServer,
+} from "./characters/constants";
 
 const app = express();
 
-app.get("/", (_, res) => res.send("hola"));
+app.set("query parser", (str: string) => parse(str));
 
-app.listen(3001);
+app.get("/count", (_, res: Response<CountResponse>) => {
+  fetch(rickAndMortyGraphQLServer, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ query: humansCountQuery }),
+  })
+    .then((_res) => _res.json())
+    .then((_res) => res.json({ count: _res.data.characters.info.count }));
+});
+
+app.get("/characters", (req, res: Response<CharactersResponse>) => {
+  const page =
+    req.query["page"] !== undefined && !Number.isNaN(req.query["page"])
+      ? (req.query["page"] as string)
+      : "1";
+  fetch(rickAndMortyGraphQLServer, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ query: humansQuery.replace(pagePlaceholder, page) }),
+  })
+    .then((_res) => _res.json())
+    .then((_res) => res.json({ characters: _res.data.characters.results }));
+});
+
+app.listen(port);
